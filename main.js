036a8e2605bb2438d7cd09f178cc869080c33b3a
Lukas Mihalciny <mihalciny@studiotem.com>
2020-04-13 15:32:11 +0200

    element layout -> html + css

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
 	// Create the browser window.
 	const win = new BrowserWindow({
-		width: 1300,
-		height: 700,
+		width: 1600,
+		height: 900,
 		webPreferences: {
 			nodeIntegration: true
 		}
