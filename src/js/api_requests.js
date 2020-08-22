a85fdb75af1cb8416f42d73d510273f064f1c859
Lukas Mihalciny <mihalciny@studiotem.com>
2020-08-22 10:47:10 +0200

    0.0.16 Api_requests.prototype.set_running_entry()

--- src/js/api_requests.js
+++ src/js/api_requests.js
@@ -6,6 +6,10 @@ import error_window from './submodules/error_window.js';
 import opened_projects from './submodules/opened_projects.js';
 import running_entry from './submodules/running_entry.js';
 
+/**
+ * CL documentation https://costlocker.docs.apiary.io/#reference/0/rest-api-v2
+*/
+
 function Api_requests() {}
 
 Api_requests.prototype.user_detail = function() {
@@ -64,12 +68,23 @@ Api_requests.prototype.get_assignments = function() {
 	var headers = new Headers();
 	headers.append( 'Authorization', constants.get_authorization() );
 	headers.append( 'Content-Type', 'application/json' );
-	var raw = '{ "Simple_Projects": {}, "Simple_Activities": {}, "Simple_Timesheet": { "datef": "2016-11-01", "datet": "2016-11-30" }, "Simple_Tracking_Assignments": {}, "Simple_Tracking_RunningEntry": {}, "Simple_Tags": {} }';
+	var post_body = {
+		'Simple_Projects': {},
+		'Simple_Activities': {},
+		'Simple_Timesheet': {
+			'datef': '2016-11-01',
+			'datet': '2016-11-30'
+		},
+		'Simple_Tracking_Assignments': {},
+		'Simple_Tracking_RunningEntry': {},
+		'Simple_Tags': {}
+	};
+	post_body = JSON.stringify( post_body );
 	var requestOptions = {
 		method: 'POST',
 		headers: headers,
 		redirect: 'follow',
-		body: raw
+		body: post_body
 	};
 	var storage = {};
 	fetch(
@@ -102,6 +117,60 @@ Api_requests.prototype.get_assignments = function() {
 				};
 				error_window.display( error_object );
 			}
+		}
+	).catch(
+		error => {
+			console.log( 'error: ' + typeof error ); console.log( error );
+		}
+	);
+}
+
+Api_requests.prototype.set_running_entry = function() {
+	var headers = new Headers();
+	headers.append( 'Authorization', constants.get_authorization() );
+	headers.append( 'Content-Type', 'application/json' );
+	var post_body = {
+		'data': [
+			{
+				'uuid': '1bbd6745-b2c3-436b-9b2f-ccd4cdd6aea6',
+				'description': 'TEST2',
+				'date': '2020-08-22T10:30:26+0000',
+				'duration': null,
+				'assignment': {
+					'person_id': 7778,
+					'project_id': 120226,
+					'activity_id': 15459,
+					'task_id': 422056
+				}
+			}
+		]
+	};
+	post_body = JSON.stringify( post_body );
+	var requestOptions = {
+		method: 'POST',
+		headers: headers,
+		redirect: 'follow',
+		body: post_body
+	};
+	var storage = {};
+	fetch(
+		'https://new.costlocker.com/api-public/v2/timeentries/',
+		requestOptions
+	).then(
+		response => {
+			// save response headers into storage
+			storage = response;
+			return response;
+		}
+	).then(
+		response => {
+			// parse response text
+			return response.text();
+		}
+	).then(
+		result => {
+			console.log( 'storage: ' + typeof storage ); console.log( storage );
+			console.log( 'result: ' + typeof result ); console.log( result );
 		}
 	).catch(
 		error => {
